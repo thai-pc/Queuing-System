6f94ef791212207c206409f359fe4c80d69bf1ce
dongthai <vodongthais1@gmail.com>
2022-01-04 19:55:39 +0700

    update

---
+++ src/components/Confirm/Confirm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Confirm() {
+  return <div></div>;
+}
+
+export default Confirm;
---
+++ src/components/Forget/Forget.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Forget() {
+  return <div></div>;
+}
+
+export default Forget;
