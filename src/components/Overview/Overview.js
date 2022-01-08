ab8510ec384639993ea16ecf43195d37cbcf8f94
dongthai <vodongthais1@gmail.com>
2022-01-08 23:39:00 +0700

    master

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -1,30 +1,67 @@
-import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import clsx from 'clsx';
 import styles from './Overview.module.scss';
+import { OverviewData } from './OverviewData';
 
 function Overview() {
   return (
     <div className={styles.box}>
-      <div className={styles.item}>
-        <div className={styles.circle}>
-          <svg>
-            <circle cx="25" cy="75" r="20" />
-          </svg>
-        </div>
-        <div className={styles.numerical}>
-          <div></div>
-        </div>
-        <div className={styles.status}>
-          <p>
-            <FiberManualRecordIcon />
-            Đang hoạt động<span>3.799</span>
-          </p>
-          <p>
-            <FiberManualRecordIcon /> Ngưng hoạt động<span>422</span>
-          </p>
-        </div>
-      </div>
-      <div className={styles.item}>2</div>
-      <div className={styles.item}>3</div>
+      {OverviewData.map((item, index) => {
+        return (
+          <div
+            className={clsx(styles.item, 'd-flex', {
+              [styles.orange]: index === 0,
+              [styles.blue]: index === 1,
+              [styles.green]: index === 2,
+            })}
+            key={index}
+          >
+            <div className={styles.circle}>
+              <svg className={styles.fisrt}>
+                <circle cx="30" cy="30" r="30" />
+                <circle cx="30" cy="30" r="30" />
+              </svg>
+              <svg className={styles.second}>
+                <circle cx="25" cy="25" r="25" />
+                <circle cx="25" cy="25" r="25" />
+              </svg>
+              {index === 2 ? (
+                <svg className={styles.third}>
+                  <circle cx="20" cy="20" r="20" />
+                  <circle cx="20" cy="20" r="20" />
+                </svg>
+              ) : null}
+
+              <span className={styles.progress}>{item.precent}</span>
+            </div>
+            <div className={styles.numerical}>
+              <div className={styles.number}>{item.number}</div>
+              <div className={styles.icon}>
+                {item.icon}
+                <span>{item.text}</span>
+              </div>
+            </div>
+            <div className={styles.status}>
+              <div className={clsx(styles.content, styles.active, 'd-flex')}>
+                {item.dots}
+                {item.active}
+                <div className={styles.number}>{item.number1}</div>
+              </div>
+              <div className={clsx(styles.content, 'd-flex')}>
+                {item.dots}
+                {item.stop}
+                <div className={styles.number}>{item.number2}</div>
+              </div>
+              {index === 2 ? (
+                <div className={clsx(styles.content, styles.skip, 'd-flex')}>
+                  {item.dots}
+                  {item.skip}
+                  <div className={styles.number}>{item.number3}</div>
+                </div>
+              ) : null}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/components/Overview/OverviewData.js
@@ -0,0 +1,42 @@
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import DesktopWindowsOutlinedIcon from '@mui/icons-material/DesktopWindowsOutlined';
+import LayersOutlinedIcon from '@mui/icons-material/LayersOutlined';
+import ForumOutlinedIcon from '@mui/icons-material/ForumOutlined';
+
+export const OverviewData = [
+  {
+    precent: '90%',
+    number: 4.221,
+    icon: <DesktopWindowsOutlinedIcon />,
+    text: 'Thiết bị',
+    dots: <FiberManualRecordIcon />,
+    active: 'Đang hoạt động',
+    number1: 3.799,
+    stop: 'Ngưng hoạt động',
+    number2: 422,
+  },
+  {
+    precent: '76%',
+    number: 276,
+    icon: <ForumOutlinedIcon />,
+    text: 'Dịch vụ',
+    dots: <FiberManualRecordIcon />,
+    active: 'Đang hoạt động',
+    number1: 210,
+    stop: 'Ngưng hoạt động',
+    number2: 66,
+  },
+  {
+    precent: '86%',
+    number: 4.221,
+    icon: <LayersOutlinedIcon />,
+    text: 'Cấp số',
+    dots: <FiberManualRecordIcon />,
+    active: 'Đã sử dụng',
+    number1: 3.721,
+    stop: 'Đang chờ',
+    number2: 486,
+    skip: 'Bỏ qua',
+    number3: 32,
+  },
+];
